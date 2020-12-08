dcee92d68cd42ac58596e71990fae27ab3bac13a
avsssai <34810623+avsssai@users.noreply.github.com>
2020-12-08 23:46:09 +0530

    sollu

--- initial.js
+++ initial.js
@@ -7,3 +7,5 @@ function sum(a, b) {
 }
 
 console.log(sum(2, 5)); //returns 7
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti doloremque vel expedita modi labore quia qui, quos mollitia quam, molestiae voluptas consequuntur esse minima, ratione non minus veritatis ad placeat.
